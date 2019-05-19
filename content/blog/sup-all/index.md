922cb8d24f4c3eadfeecc1149f59d2a211ea341a
Jenn Eng <jenneng6@gmail.com>
2019-05-19 12:35:07 -0400

    overwrite some of the demo stuff

---
+++ content/blog/sup-all/index.md
@@ -0,0 +1,7 @@
+---
+date: 2019-05-19T17:12:33.962Z
+title: "Sup y'all"
+description: "im not a robot"
+---
+
+Boop beep boop
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    author: `JEng`,
+    description: `A starter blog exploring gatsby`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `lol`,
     },
   },
   plugins: [
